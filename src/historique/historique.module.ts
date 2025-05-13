898c598b2a8a111770e97b767ee8db6a70d48364
Bacha Med Amine <medamine.bacha@esprit.tn>
2025-05-13 21:03:00 +0100

    fixing historique

--- src/historique/historique.module.ts
+++ src/historique/historique.module.ts
@@ -9,6 +9,7 @@ import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { PainCheckService } from './pain-check.service';
 import { NotificationModule } from 'src/notification/notification.module';
+import { User, UserSchema } from 'src/auth/schema/user.schema';
 
 @Module({
   imports: [
@@ -20,11 +21,17 @@ import { NotificationModule } from 'src/notification/notification.module';
         signOptions: { expiresIn: '1d' },
       }),
     }),
-    MongooseModule.forFeature([{ name: Historique.name, schema: HistoriqueSchema }]),
+    MongooseModule.forFeature([{ name: Historique.name, schema: HistoriqueSchema }
+      ,
+      {
+        name: User.name,
+        schema: UserSchema
+      },
+    ]),
     HttpModule,
     NotificationModule,
   ],
   controllers: [HistoriqueController],
-  providers: [HistoriqueService, FileUploadService, PainCheckService], // Ajout du service ici
+  providers: [HistoriqueService, FileUploadService, PainCheckService], // 🔹 Ajout du service ici
 })
 export class HistoriqueModule {}
--- src/main.ts
+++ src/main.ts
@@ -11,49 +11,49 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const configService = app.get(ConfigService);
+const app = await NestFactory.create(AppModule);
+const configService = app.get(ConfigService);
 
-  app.use('/uploads', express.static(join(__dirname, '..', 'Uploads')));
-  app.useGlobalPipes(new ValidationPipe());
+app.use('/uploads', express.static(join(__dirname, '..', 'Uploads')));
+app.useGlobalPipes(new ValidationPipe());
 
-  const config = new DocumentBuilder()
-    .setTitle('MsAware 2.0')
-    .setDescription(
-      'API documentation for BlackMirror : A platform for monitoring and managing healthcare industry professionals and patients'
-    )
-    .setVersion('1.0')
-    .addBearerAuth()
-    .build();
+const config = new DocumentBuilder()
+.setTitle('MsAware 2.0')
+.setDescription(
+'API documentation for BlackMirror : A platform for monitoring and managing healthcare industry professionals and patients'
+)
+.setVersion('1.0')
+.addBearerAuth()
+.build();
 
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api/docs', app, document);
+const document = SwaggerModule.createDocument(app, config);
+SwaggerModule.setup('api/docs', app, document);
 
-  const pythonRunnerService = app.get(PythonRunnerService);
-  pythonRunnerService.startAiModelServer();
-  pythonRunnerService.startClassifierServer();
-  pythonRunnerService.startRegressorServer();
+const pythonRunnerService = app.get(PythonRunnerService);
+pythonRunnerService.startAiModelServer();
+pythonRunnerService.startClassifierServer();
+pythonRunnerService.startRegressorServer();
 
-  process.on('SIGINT', async () => {
-    console.log('Shutting down NestJS application...');
-    pythonRunnerService.stopAllServers();
-    await new Promise((resolve) => setTimeout(resolve, 10000)); // Wait 10 seconds
-    await app.close();
-    process.exit(0);
-  });
+process.on('SIGINT', async () => {
+console.log('Shutting down NestJS application...');
+pythonRunnerService.stopAllServers();
+await new Promise((resolve) => setTimeout(resolve, 10000)); // Wait 10 seconds
+await app.close();
+process.exit(0);
+});
 
-  process.on('SIGTERM', async () => {
-    console.log('Shutting down NestJS application...');
-    pythonRunnerService.stopAllServers();
-    await new Promise((resolve) => setTimeout(resolve, 10000)); // Wait 10 seconds
-    await app.close();
-    process.exit(0);
-  });
+process.on('SIGTERM', async () => {
+console.log('Shutting down NestJS application...');
+pythonRunnerService.stopAllServers();
+await new Promise((resolve) => setTimeout(resolve, 10000)); // Wait 10 seconds
+await app.close();
+process.exit(0);
+});
 
-  const port = process.env.PORT ?? 3000;
-  await app.listen(port, '0.0.0.0');
-  console.log(`Server is running on http://localhost:${port}`);
-  console.log(`Swagger docs available at http://localhost:${port}/api/docs`);
+const port = process.env.PORT ?? 3000;
+await app.listen(port, '0.0.0.0');
+console.log(`Server is running on http://localhost:${port}`);
+console.log(`Swagger docs available at http://localhost:${port}/api/docs`);
 }
 
 bootstrap();
